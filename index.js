96e0dacdc1a4c62482bea43e355d882f4ec3822a
Kyrylo <fresh.js.ftd.@gmail.com>
2022-11-03 21:03:01 +0200

    Add read file example

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 // CommonJS import
-const importedData = require('./MyMath');
-const MyComponent = require('./MyComponent');
-console.log('index');
+// const importedData = require('./MyMath');
+// const MyComponent = require('./MyComponent');
+// console.log('index');
 
+const fs = require('fs/promises');
+// const fs = require('fs').promises;
+
+// fs.readFile('./text.txt', 'utf-8').then(console.log);
+fs.readFile('./text.txt', 'utf-8').then((text) => {
+  console.log(text);
+});
 
 // 1 resolving - ищет ваш файл
-  // 1.1 включаются core модули ноды
-  // 1.2 ищет файл по пути из строки в require
-    // 1.2.1 ищется файл с расширением js
-    // 1.2.2 ищется файл с расширением json
-  // 1.3 ищется папка с именем из require
-    // 1.3.1 package.json -> "main"
-    // 1.3.2 index.js
-    // 1.3.3 index.json
-  // 1.4 смотрит, есть ли папка 
-  // 1.5 кидаем ошибку, нечего импортировать
+// 1.1 включаются core модули ноды
+// 1.2 ищет файл по пути из строки в require
+// 1.2.1 ищется файл с расширением js
+// 1.2.2 ищется файл с расширением json
+// 1.3 ищется папка с именем из require
+// 1.3.1 package.json -> "main"
+// 1.3.2 index.js
+// 1.3.3 index.json
+// 1.4 смотрит, есть ли папка
+// 1.5 кидаем ошибку, нечего импортировать
 // 2 loading - читаем файл
 // 3 wrapping - оборачиват файл в функцию, при этом добавляет
 //    тада все injected (require, __dirname, console ... )
